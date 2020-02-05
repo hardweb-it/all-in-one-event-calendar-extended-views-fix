9ef3a5e3be28415f27c0486ed689a3d0a722d44e
Gianluca Marzaro <3406023+hardweb-it@users.noreply.github.com>
2020-02-05 15:50:47 +0100

    0.4 version upload

--- all-in-one-event-calendar-extended-views-fix.php
+++ all-in-one-event-calendar-extended-views-fix.php
@@ -14,7 +14,7 @@
  * @wordpress-plugin
  * Plugin Name:       All-in-One Event Calendar Extended Views FIX
  * Description:       Dynamically re-enable the plugin if it's disabled.
- * Version:           0.2
+ * Version:           0.4
  * Author:            Hardweb.it
  * Author URI:        https://hardweb.it/
  * License:           GPL-2.0+
@@ -28,26 +28,29 @@ if (!defined('WPINC')) {
 }
 
 #FIX AIO Event Calendar Extended Views automatically disable itself
-
-if (!is_admin()) {
-	add_action('plugins_loaded', 'hw_fix_aioecev_disable');
-	function hw_fix_aioecev_disable() {
-		hw_fix_aioecev_enable_extension();
+add_action('init', 'hw_fix_aioecev_smart_check');
+function hw_fix_aioecev_smart_check() {
+	if (isset($_GET['post'])) {
+		$post_id = esc_attr($_GET['post']);
+		if (get_post_type($post_id) == 'ai1ec_event') {
+			hw_fix_aioecev_disable_extension();
+		}
 	}
-} else {
-	add_action('muplugins_loaded','hw_fix_aioecev_disable_extension');
-	add_action('save_post','hw_fix_aioecev_enable_extension');
 }
+
+add_action('get_header', 'hw_fix_aioecev_enable_extension');
 
 function hw_fix_aioecev_enable_extension() {
 	if ( !is_plugin_active( 'all-in-one-event-calendar-extended-views/all-in-one-event-calendar-extended-views.php' ) ) {
 		activate_plugin('all-in-one-event-calendar-extended-views/all-in-one-event-calendar-extended-views.php');
+		//error_log(print_r("ai1ec_extension enabled", true));
 	}
 }
 
 function hw_fix_aioecev_disable_extension() {
 	if ( is_plugin_active( 'all-in-one-event-calendar-extended-views/all-in-one-event-calendar-extended-views.php' ) ) {
 		deactivate_plugins('all-in-one-event-calendar-extended-views/all-in-one-event-calendar-extended-views.php');
+		//error_log(print_r("ai1ec_extension disabled", true));
 	}
 }
 ?>
